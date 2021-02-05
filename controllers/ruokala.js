43f2f9c6d4b9c338a96542baabf15e91325f5f1f
unknown <yaramodesu@gmail.com>
2021-02-05 07:21:44 +0200

    ruokalan ja kassan routet ja controllerit tehty

--- controllers/ruokala.js
+++ controllers/ruokala.js
@@ -1,5 +1,6 @@
 const { ruokaSchema} = require('../schemas.js')
 const Ruoka = require('../models/ruoka')
+const Tilaus = require('../models/tilaus')
 
 module.exports.index = async(req, res) => {
     const tuotteet = await Ruoka.find({})
@@ -28,4 +29,40 @@ module.exports.uusiRuokaFormi = (req, res) => {
     await Ruoka.findByIdAndDelete(id)
     res.redirect('/ruokala/kassa')
 }
+module.exports.kassa = async (req, res) => {
+    const tuotteet = await Ruoka.find({})
+    const tilaukset = await Tilaus.find({}).populate('tilaaja')
+    res.render('ruokala/kassa', { tuotteet, tilaukset})
+}
 
+module.exports.moukkaaRuokaMäärää = async (req, res) => {
+    const { id } = req.params
+    const update = await Ruoka.findByIdAndUpdate(id, {...req.body.ruokala})
+    res.redirect('/ruokala/kassa')
+}
+
+module.exports.uusiTilaaFormi = async(req,res) => {
+    const tuotteet = await Ruoka.find({})
+    res.render('ruokala/tilaa', { tuotteet })
+}
+
+module.exports.luoUusiTilaus = async(req, res) => {
+    const { id } =req.params
+    const tilaus = req.body.tilaus * -1
+    const uusiMäärä = await Ruoka.findByIdAndUpdate(id, { $inc: {'määrä': `${tilaus}`}})
+    console.log(tilaus)
+    const ruoka = await Ruoka.findById(id)
+    const uusiTilaus = new Tilaus()
+    uusiTilaus.tuote = ruoka.nimi
+    uusiTilaus.määrä = req.body.tilaus
+    uusiTilaus.tilaaja = req.user._id
+    await uusiTilaus.save()
+    res.redirect('/ruokala')
+}
+
+module.exports.merkitseTilausVastaanotetuksi = async(req, res) => {
+   
+    const { id } = req.params
+    await Tilaus.findByIdAndDelete(id)
+    res.redirect('/ruokala/kassa')
+}
--- routes/ruokala.js
+++ routes/ruokala.js
@@ -2,14 +2,11 @@ const express = require('express')
 //houmaa merge params
 const router = express.Router()
 const ruokala = require('../controllers/ruokala')
-const Ruoka = require('../models/ruoka')
-const Tilaus = require('../models/tilaus')
+
 
 //errorit ja validointi
 const catchAsync = require('../utils/catchAsync')
 const {onKirjautunut, validoiRuoka, onAdmin} = require('../middleware')
-const renderFile = require('ejs-mate')
-
 
 
 
@@ -19,50 +16,17 @@ router.get('/', catchAsync(ruokala.index))
 router.post('/' ,validoiRuoka, catchAsync(ruokala.luoUusiRuokaMyyntiin))
 // router.get('/tilaa', onKirjautunut, ruokala.tilaaRuokaa)
 
-router.get('/kassa', onAdmin, onKirjautunut ,catchAsync(async (req, res) => {
-    const tuotteet = await Ruoka.find({})
-    const tilaukset = await Tilaus.find({}).populate('tilaaja')
-    res.render('ruokala/kassa', { tuotteet, tilaukset})
-}))
+router.get('/kassa', onAdmin, onKirjautunut ,catchAsync(ruokala.kassa))
 
-router.put('/kassa/:id', async (req, res) => {
-    const { id } = req.params
-    const update = await Ruoka.findByIdAndUpdate(id, {...req.body.ruokala})
-    res.redirect('/ruokala/kassa')
-})
+router.put('/kassa/:id', onAdmin, catchAsync(ruokala.moukkaaRuokaMäärää))
 
+router.get('/uusi', onAdmin, ruokala.uusiRuokaFormi)
 
+router.get('/tilaa', onKirjautunut, catchAsync(ruokala.uusiTilaaFormi))
 
+router.put('/tilaa/:id', onKirjautunut, catchAsync( ruokala.luoUusiTilaus))
 
-router.get('/uusi', ruokala.uusiRuokaFormi)
-
-
-
-router.get('/tilaa', catchAsync(async(req,res) => {
-    const tuotteet = await Ruoka.find({})
-    res.render('ruokala/tilaa', { tuotteet })
-}))
-
-router.put('/tilaa/:id', catchAsync( async(req, res) => {
-    const { id } =req.params
-    const tilaus = req.body.tilaus * -1
-    const uusiMäärä = await Ruoka.findByIdAndUpdate(id, { $inc: {'määrä': `${tilaus}`}})
-    console.log(tilaus)
-    const ruoka = await Ruoka.findById(id)
-    const uusiTilaus = new Tilaus()
-    uusiTilaus.tuote = ruoka.nimi
-    uusiTilaus.määrä = req.body.tilaus
-    uusiTilaus.tilaaja = req.user._id
-    await uusiTilaus.save()
-    res.redirect('/ruokala')
-}))
-
-router.delete('/kassa/:id', catchAsync(async(req, res) => {
-   
-    const { id } = req.params
-    await Tilaus.findByIdAndDelete(id)
-    res.redirect('/ruokala/kassa')
-} ))
+router.delete('/kassa/:id', onAdmin, catchAsync(ruokala.merkitseTilausVastaanotetuksi ))
 
 router.delete('/:id', onKirjautunut, onAdmin, catchAsync(ruokala.poistaRuokaMyynistä))
 
