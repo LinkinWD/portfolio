069425ca87ce04b27fda18fed9da4f6efa5da106
unknown <yaramodesu@gmail.com>
2021-02-08 11:24:52 +0200

    pallot lisätty, henkkari hyvä

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -55,3 +55,32 @@ const chart = new Chart(donitsi, {
         }
     }
 });
+
+function randomValues() {
+	anime({
+		targets: '.pallo1, .pallo2',
+
+		translateX: function() {
+			return anime.random(-30, 30) + 'vw';
+		},
+		translateY: function() {
+			return anime.random(-30, 30) + 'vh';
+		},
+		scale: function() {
+			return anime.random(10, 30) / 10;
+		},
+		background: function() {
+			let R = anime.random(0, 255);
+			let G = anime.random(0, 255);
+			let B = anime.random(0, 255);
+			let vari = `rgb(${R},${G},${B})`;
+			return vari;
+		},
+		easing: 'easeInOutQuad',
+		duration: 750,
+		complete: randomValues
+	});
+}
+
+randomValues();
+
