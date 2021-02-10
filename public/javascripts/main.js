b0d50a2ee88bc3bdd029c1e5cef31be5c878e11d
unknown <yaramodesu@gmail.com>
2021-02-10 09:20:58 +0200

    alkusivu alkaa oleen melkein ok

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,32 +2,6 @@
 const donitsi = document.getElementById('donitsi');
 
 
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.otsikko .kirjaimet');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline()
-.add({
-    targets: '.otsikko',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  })
-  .add({
-    targets: '.otsikko',
-    opacity: 1,
-    duration: 100,
-    easing: "easeOutExpo",
-    delay: 100
-  })
-  .add({
-    targets: '.otsikko .kirjaimet',
-    scale: [0, 1],
-    duration: 1500,
-    elasticity: 600,
-    delay: (el, i) => 45 * (i+1)
-  });
 
 
 
@@ -89,24 +63,24 @@ const chart = new Chart(donitsi, {
 
 function randomValues() {
 	anime({
-		targets: '.pallo1, .pallo2',
+		targets: '.taustapallo1, .taustapallo2, .taustapallo3, .taustapallo4',
 
 		translateX: function() {
-			return anime.random(-30, 30) + 'vw';
+			return anime.random(-10, 10) + 'vw';
 		},
 		translateY: function() {
-			return anime.random(-30, 30) + 'vh';
+			return anime.random(-10, 10) + 'vh';
 		},
-		scale: function() {
+		/* scale: function() {
 			return anime.random(10, 30) / 10;
-		},
-		background: function() {
+		}, */
+		/* background: function() {
 			let R = anime.random(0, 255);
 			let G = anime.random(0, 255);
 			let B = anime.random(0, 255);
 			let vari = `rgb(${R},${G},${B})`;
 			return vari;
-		},
+		}, */
 		easing: 'easeInOutQuad',
 		duration: 750,
 		complete: randomValues
