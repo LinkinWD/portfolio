068e672702b5a68fec94e458714b409b77bfd07b
unknown <yaramodesu@gmail.com>
2021-02-12 17:14:21 +0200

    pian valmis

---
+++ public/javascripts/rangemuuttuja.js
@@ -0,0 +1,7 @@
+let range = document.getElementById('range')
+let tulos = document.getElementById('tulos')
+
+let määrä = range.value 
+
+range.addEventListener.apply('click', () => {
+tulos.innerText = määrä})
